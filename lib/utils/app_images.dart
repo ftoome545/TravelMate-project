730aaa5ea02cc29b41b24f287a84a0ae447a9a37
FatimaHure <ftoome7767@hotmail.com>
2024-10-16 22:17:59 +0300

    add app assets

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,181 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAhmedGuideCircleImage
+  /// assets/images/Ahmed_guide_circle_image.svg
+  static const String imagesAhmedGuideCircleImage = "assets/images/Ahmed_guide_circle_image.svg";
+
+  /// Assets for imagesArrowRightIcon
+  /// assets/images/arrow_right_icon.svg
+  static const String imagesArrowRightIcon = "assets/images/arrow_right_icon.svg";
+
+  /// Assets for imagesBackIcon
+  /// assets/images/back_icon.svg
+  static const String imagesBackIcon = "assets/images/back_icon.svg";
+
+  /// Assets for imagesBrowseGuidesImage
+  /// assets/images/browse_guides_image.svg
+  static const String imagesBrowseGuidesImage = "assets/images/browse_guides_image.svg";
+
+  /// Assets for imagesCalendarIcon
+  /// assets/images/calendar_icon.svg
+  static const String imagesCalendarIcon = "assets/images/calendar_icon.svg";
+
+  /// Assets for imagesCheckIcon
+  /// assets/images/check-icon.svg
+  static const String imagesCheckIcon = "assets/images/check-icon.svg";
+
+  /// Assets for imagesChooseGenderCircleIcon
+  /// assets/images/choose_gender_circle_icon.svg
+  static const String imagesChooseGenderCircleIcon = "assets/images/choose_gender_circle_icon.svg";
+
+  /// Assets for imagesChooseGuideImage
+  /// assets/images/choose_guide_image.svg
+  static const String imagesChooseGuideImage = "assets/images/choose_guide_image.svg";
+
+  /// Assets for imagesCloseIcon
+  /// assets/images/close_icon.svg
+  static const String imagesCloseIcon = "assets/images/close_icon.svg";
+
+  /// Assets for imagesCreateTripImage
+  /// assets/images/create_trip_image.svg
+  static const String imagesCreateTripImage = "assets/images/create_trip_image.svg";
+
+  /// Assets for imagesDeleteIcon
+  /// assets/images/delete_icon.svg
+  static const String imagesDeleteIcon = "assets/images/delete_icon.svg";
+
+  /// Assets for imagesFacebookIcon
+  /// assets/images/facebook_icon.svg
+  static const String imagesFacebookIcon = "assets/images/facebook_icon.svg";
+
+  /// Assets for imagesFemaleIcon
+  /// assets/images/female_icon.svg
+  static const String imagesFemaleIcon = "assets/images/female_icon.svg";
+
+  /// Assets for imagesGoogleIcon
+  /// assets/images/google_icon.svg
+  static const String imagesGoogleIcon = "assets/images/google_icon.svg";
+
+  /// Assets for imagesGuideImageInConfirmScreen
+  /// assets/images/guide_image_in_confirm_screen.svg
+  static const String imagesGuideImageInConfirmScreen = "assets/images/guide_image_in_confirm_screen.svg";
+
+  /// Assets for imagesGuideOrTouristImage
+  /// assets/images/guide_or_tourist_image.svg
+  static const String imagesGuideOrTouristImage = "assets/images/guide_or_tourist_image.svg";
+
+  /// Assets for imagesGuideSarahImage
+  /// assets/images/guide_Sarah_image.svg
+  static const String imagesGuideSarahImage = "assets/images/guide_Sarah_image.svg";
+
+  /// Assets for imagesHomeIcon
+  /// assets/images/home_icon.svg
+  static const String imagesHomeIcon = "assets/images/home_icon.svg";
+
+  /// Assets for imagesMailIcon
+  /// assets/images/mail_icon.svg
+  static const String imagesMailIcon = "assets/images/mail_icon.svg";
+
+  /// Assets for imagesMale
+  /// assets/images/male.svg
+  static const String imagesMale = "assets/images/male.svg";
+
+  /// Assets for imagesMessagesIcon
+  /// assets/images/messages_icon.svg
+  static const String imagesMessagesIcon = "assets/images/messages_icon.svg";
+
+  /// Assets for imagesMessagesScreenIcon
+  /// assets/images/messages_screen_icon.svg
+  static const String imagesMessagesScreenIcon = "assets/images/messages_screen_icon.svg";
+
+  /// Assets for imagesMoreIcon
+  /// assets/images/more_icon.svg
+  static const String imagesMoreIcon = "assets/images/more_icon.svg";
+
+  /// Assets for imagesMyLocationImage
+  /// assets/images/my_location_image.svg
+  static const String imagesMyLocationImage = "assets/images/my_location_image.svg";
+
+  /// Assets for imagesNewTripImageScreen
+  /// assets/images/new_trip_image_screen.svg
+  static const String imagesNewTripImageScreen = "assets/images/new_trip_image_screen.svg";
+
+  /// Assets for imagesOptionsIcon
+  /// assets/images/options_icon.svg
+  static const String imagesOptionsIcon = "assets/images/options_icon.svg";
+
+  /// Assets for imagesPlusAddIcon
+  /// assets/images/plus_add_icon.svg
+  static const String imagesPlusAddIcon = "assets/images/plus_add_icon.svg";
+
+  /// Assets for imagesProfileAvatarImage
+  /// assets/images/profile_avatar_image.svg
+  static const String imagesProfileAvatarImage = "assets/images/profile_avatar_image.svg";
+
+  /// Assets for imagesProfileIcon
+  /// assets/images/profile_icon.svg
+  static const String imagesProfileIcon = "assets/images/profile_icon.svg";
+
+  /// Assets for imagesQuoteIcon1
+  /// assets/images/quote_icon_1.svg
+  static const String imagesQuoteIcon1 = "assets/images/quote_icon_1.svg";
+
+  /// Assets for imagesQuoteIcon2
+  /// assets/images/quote_icon_2.svg
+  static const String imagesQuoteIcon2 = "assets/images/quote_icon_2.svg";
+
+  /// Assets for imagesRatingStarsConfirmScreen
+  /// assets/images/rating_stars_confirm_screen.svg
+  static const String imagesRatingStarsConfirmScreen = "assets/images/rating_stars_confirm_screen.svg";
+
+  /// Assets for imagesRatingStarsImage
+  /// assets/images/rating_stars_image.svg
+  static const String imagesRatingStarsImage = "assets/images/rating_stars_image.svg";
+
+  /// Assets for imagesSarahCircleImage
+  /// assets/images/Sarah_circle_image.svg
+  static const String imagesSarahCircleImage = "assets/images/Sarah_circle_image.svg";
+
+  /// Assets for imagesSearchIcon
+  /// assets/images/search_icon.svg
+  static const String imagesSearchIcon = "assets/images/search_icon.svg";
+
+  /// Assets for imagesSearchIconHomePage
+  /// assets/images/search_icon_home_page.svg
+  static const String imagesSearchIconHomePage = "assets/images/search_icon_home_page.svg";
+
+  /// Assets for imagesSelectGenderImage
+  /// assets/images/select_gender_image.svg
+  static const String imagesSelectGenderImage = "assets/images/select_gender_image.svg";
+
+  /// Assets for imagesSendIcon
+  /// assets/images/send_icon.svg
+  static const String imagesSendIcon = "assets/images/send_icon.svg";
+
+  /// Assets for imagesShowPasswordIcon
+  /// assets/images/show_password_icon.svg
+  static const String imagesShowPasswordIcon = "assets/images/show_password_icon.svg";
+
+  /// Assets for imagesSubmitOffersImage
+  /// assets/images/submit_offers_image.svg
+  static const String imagesSubmitOffersImage = "assets/images/submit_offers_image.svg";
+
+  /// Assets for imagesTravelMateLogo
+  /// assets/images/travelMate_logo.svg
+  static const String imagesTravelMateLogo = "assets/images/travelMate_logo.svg";
+
+  /// Assets for imagesTripIcon
+  /// assets/images/trip_icon.svg
+  static const String imagesTripIcon = "assets/images/trip_icon.svg";
+
+  /// Assets for imagesTripSendIcon
+  /// assets/images/trip_send_icon.svg
+  static const String imagesTripSendIcon = "assets/images/trip_send_icon.svg";
+
+  /// Assets for imagesXIcon
+  /// assets/images/x_icon.svg
+  static const String imagesXIcon = "assets/images/x_icon.svg";
+}
+
